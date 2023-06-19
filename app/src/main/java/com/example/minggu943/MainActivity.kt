0f43ebe2559957571703615f0e674d25ed8ce808
yolaCode <violetkimmelatte@gmail.com>
2023-06-19 18:05:55 +0700

    MP4.3

--- app/src/main/java/com/example/minggu943/MainActivity.kt
+++ app/src/main/java/com/example/minggu943/MainActivity.kt
@@ -31,15 +31,15 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_item1 -> {
-                tvSelectedOption.text = "Selected Option: Option 1"
+                tvSelectedOption.text = "Your Favorite Boy: Jaehyun"
                 return true
             }
             R.id.menu_item2 -> {
-                tvSelectedOption.text = "Selected Option: Option 2"
+                tvSelectedOption.text = "Your Favorite Boy: Jeno"
                 return true
             }
             R.id.menu_item3 -> {
-                tvSelectedOption.text = "Selected Option: Option 3"
+                tvSelectedOption.text = "Your Favorite Boy: Jay"
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
@@ -51,13 +51,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showPicker() {
-        val pickerOptions = arrayOf("Option 1", "Option 2", "Option 3")
+        val pickerOptions = arrayOf("Jaehyun", "Jeno", "Jay")
 
         val builder = androidx.appcompat.app.AlertDialog.Builder(this)
-        builder.setTitle("Select an option")
+        builder.setTitle("Your Favorite Boy")
 
         builder.setItems(pickerOptions) { _, which ->
-            tvSelectedOption.text = "Selected Option: ${pickerOptions[which]}"
+            tvSelectedOption.text = "Your Favorite Boy: ${pickerOptions[which]}"
         }
 
         builder.create().show()
